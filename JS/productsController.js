9e77b69fc67d490896efc62bcc72621c2527bd72
anuradhakumar <k_anu_bpt@yahoo.co.in>
2021-12-08 16:15:49 -0500

    changed the names

--- JS/productsController.js
+++ JS/productsController.js
@@ -1,22 +1,26 @@
 // Create a ItemsController class
-class ItemsController {
+class productsController {
     // Set up the items and currentId property in the contructor
     constructor(currentId = 0) {
-        this.items = [];
+        this.productArray= [];
         this.currentId = currentId;
     }
 
     // Create the addItem method
-    addItem(name, description,imageUrl) {
-        const item = {
+    addItem(pName,imageUrl,price, description) {
+        const product = {
             // Increment the currentId property
             id: this.currentId++,
-            name: name,
-            description: description,
-            imageUrl: imageUrl
+            pName,
+            imageUrl,
+            price,
+            description,
+           
         };
 
         // Push the item to the items property
-        this.items.push(item);
+        this.productArray.push(product);
     }
 }
+
+console.log()
